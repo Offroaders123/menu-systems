191724f63a62781cc450251cb8c400f83077c6b5
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-09-17 10:35:55 -0700

    Initial Commit
    Another thing I've been trying to figure out for a while, is menu declarations, whether it should be component and or HTML-driven, or if it should purely be a JS-defined thing.
    
    Either way, if I were to hypothetically use a builder function (seems like a neat way to apply for this scenario), it seems to nicely chain things together.
    
    Today's discovery was realizing how to strictly-type accelerator keyboard combinations. I very much enjoy this. There might be a better way to type this out, I'm not totally sure. This works great already though.
    
    I think part of what I'm curious about, is to define the menu using state-like objects, rather than element components themselves. This seems to be a great breakthrough I think, as then you aren't coupling it to the UI rendering itself, something I have struggled with in terms of conceptualization, and in practice.
    
    I think we're landing so I gotta go!

---
+++ menu-systems-revised.ts
@@ -0,0 +1,20 @@
+import { createMenu, type Menu } from "menu-systems";
+
+declare function createEditor(): void;
+declare function openEditor(): void;
+declare function saveEditor(): void;
+declare function saveEditorAs(): void;
+
+const file: Menu = createMenu()
+  .item("New", "Super+N", () => {
+    createEditor();
+  })
+  .item("Open", "Super+O", () => {
+    openEditor();
+  })
+  .item("Save", "Super+S", () => {
+    saveEditor();
+  })
+  .item("Save As...", "Super+Shift+S", () => {
+    saveEditorAs();
+  });
