4bed6886b14847335d39fa38329f0911f013ad0f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-09-20 19:10:45 -0400

    Template Key Types

--- accelerator.ts
+++ accelerator.ts
@@ -1,14 +1,20 @@
 export const MetaKey = ["Super", "Cmd", "Ctrl"] as const;
+export type MetaKey = typeof MetaKey[number];
 
 export const ShiftKey = "Shift";
+export type ShiftKey = typeof ShiftKey;
 
 export const AltKey = "Alt";
+export type AltKey = typeof AltKey;
 
-export const AlphabeticKey: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+export const AlphabeticKey = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] as const;
+export type AlphabeticKey = typeof AlphabeticKey[number];
 
-export const NumericKey: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+export const NumericKey = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
+export type NumericKey = typeof NumericKey[number];
 
-export const FigureKey: (string | number)[] = [...AlphabeticKey, ...NumericKey];
+export const FigureKey: (AlphabeticKey | NumericKey)[] = [...AlphabeticKey, ...NumericKey];
+export type FigureKey = typeof FigureKey[number];
 
 export const Accelerator: ((fig: string) => string[])[] = [
   fig => MetaKey.map(m => `${m}+${fig}`),
