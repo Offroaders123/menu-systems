53f42da82cc318a9e7b3ac24a4416a468d0ee48b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-09-20 18:07:12 -0400

    Demos Removal
    I plan to merge this back into keyboard too. I guess I could just have a plain accelerator module too though.

--- accelerator.ts
+++ accelerator.ts
@@ -4,7 +4,6 @@ export const ShiftKey = "Shift";
 
 export const AltKey = "Alt";
 
-// Array.from({ length: 26 }, (_,i) => i + 65).map(c => String.fromCharCode(c))
 export const AlphabeticKey: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 export const NumericKey: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -19,30 +18,6 @@ export const Accelerator: ((fig: string) => string[])[] = [
   fig => MetaKey.map(m => `${m}+${AltKey}+${fig}`),
   fig => MetaKey.map(m => `${m}+${AltKey}+${ShiftKey}+${fig}`),
 ];
-
-// for (const abc of FigureKey) {
-//   for (const accs of Accelerator) {
-//     for (const acc of accs(`${abc}`)) {
-//       console.log(acc);
-//     }
-//   }
-// }
-
-// function generateAccelerators(): string[] {
-//   const accelerators: string[] = [];
-
-//   for (const abc of FigureKey) {
-//     for (const accs of Accelerator) {
-//       for (const acc of accs(`${abc}`)) {
-//         accelerators.push(acc);
-//       }
-//     }
-//   }
-
-//   accelerators.sort();
-
-//   return accelerators;
-// }
 
 function generateAccelerators(): string[] {
   return FigureKey
