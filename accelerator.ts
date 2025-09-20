c51c643e49be84a37453e6c8aa1c4eca76a22d6c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-09-20 17:53:38 -0400

    Zero Dedupe

--- accelerator.ts
+++ accelerator.ts
@@ -7,7 +7,7 @@ export const AltKey = "Alt";
 // Array.from({ length: 26 }, (_,i) => i + 65).map(c => String.fromCharCode(c))
 export const AlphabeticKey: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-export const NumericKey: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+export const NumericKey: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export const FigureKey: (string | number)[] = [...AlphabeticKey, ...NumericKey];
 
--- keyboard.ts
+++ keyboard.ts
@@ -15,6 +15,6 @@ export type AltKey = "Alt";
 // Array.from({ length: 26 }, (_,i) => i + 65).map(c => String.fromCharCode(c))
 export type AlphabeticKey = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z";
 
-export type NumericKey = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0;
+export type NumericKey = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 export type FigureKey = AlphabeticKey | NumericKey;
