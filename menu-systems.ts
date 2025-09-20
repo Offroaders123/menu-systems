2a650dc46b00ecb8edb3a2f3463816bc6b47a49a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-09-20 17:24:21 -0400

    Keyboard Module
    Doing this because I want to start implementing things around how one would actually implement keyboard handling, like with the keybind strings.
    
    Selling England by the Pound
    
    Nice ear refresher, I like the dynamics, and changing of styles of this album. This is a great album to play Minecraft too, same with Tommy. Can't wait to sit at home and play MC 1.2 again, it's awesome with my overear Sony headphones too, I do miss those, I am just using earbuds from Target on this trip, my other ones were 6 years old, and got fuzzy when you moved them the wrong way, and sometimes just gave out when you moved the cable. So these new ones aren't as good as quality, but I also like that I can learn how certain mixes and masters sound on less than ideal headphones as well. So a nice tradeoff.
    
    Mellotrons are awesome. Very spooky.

--- menu-systems.ts
+++ menu-systems.ts
@@ -1,27 +1,7 @@
+import { type Accelerator } from "./keyboard.ts";
+
 export interface Menu {
   item(label: string, accelerator: Accelerator, onclick: () => void): Menu;
 }
-
-export type Accelerator =
-  | `${MetaKey}+${FigureKey}`
-  | `${AltKey}+${FigureKey}`
-  | `${MetaKey}+${ShiftKey}+${FigureKey}`
-  | `${AltKey}+${ShiftKey}+${FigureKey}`
-  | `${MetaKey}+${AltKey}+${FigureKey}`
-  | `${AltKey}+${ShiftKey}+${FigureKey}`
-  | `${MetaKey}+${AltKey}+${ShiftKey}+${FigureKey}`;
-
-export type MetaKey = "Super" | "Cmd" | "Ctrl";
-
-export type ShiftKey = "Shift";
-
-export type AltKey = "Alt";
-
-// Array.from({ length: 26 }, (_,i) => i + 65).map(c => String.fromCharCode(c))
-export type AlphabeticKey = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z";
-
-export type NumericKey = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0;
-
-export type FigureKey = AlphabeticKey | NumericKey;
 
 export declare function createMenu(): Menu;
