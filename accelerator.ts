1847f8163b051e2a4f9978aca71e4b62f7efaba5
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-09-20 17:47:40 -0400

    Accelerator Mapping
    Working out how to get a runtime list of values.
    
    I love the Epping Forest song. It's a vibe, a bop, and a trip! Heck yeah mr. joe! (what?)

---
+++ accelerator.ts
@@ -0,0 +1,30 @@
+export const MetaKey = ["Super", "Cmd", "Ctrl"] as const;
+
+export const ShiftKey = "Shift";
+
+export const AltKey = "Alt";
+
+// Array.from({ length: 26 }, (_,i) => i + 65).map(c => String.fromCharCode(c))
+export const AlphabeticKey: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+export const NumericKey: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+export const FigureKey: (string | number)[] = [...AlphabeticKey, ...NumericKey];
+
+export const Accelerator: ((fig: string) => string[])[] = [
+  fig => MetaKey.map(m => `${m}+${fig}`),
+  fig => [`${AltKey}+${fig}`],
+  fig => MetaKey.map(m => `${m}+${ShiftKey}+${fig}`),
+  fig => [`${AltKey}+${ShiftKey}+${fig}`],
+  fig => MetaKey.map(m => `${m}+${AltKey}+${fig}`),
+  fig => [`${AltKey}+${ShiftKey}+${fig}`],
+  fig => MetaKey.map(m => `${m}+${AltKey}+${ShiftKey}+${fig}`),
+];
+
+for (const abc of FigureKey) {
+  for (const accs of Accelerator) {
+    for (const acc of accs(`${abc}`)) {
+      console.log(acc);
+    }
+  }
+}
