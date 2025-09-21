9e65916fdcbdd74a745f62c33c859248794b3fdc
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-09-20 20:40:50 -0400

    Builder Types and Constants for Accelerators
    Okay so it does involve a bit more maps, but I think this makes a bit more sense. Will look into whether the extra mapping is (not?) worth it.
    
    I diffed the values as well, and everything seems to resolve to the same values, so that's good too.

--- accelerator.ts
+++ accelerator.ts
@@ -16,26 +16,23 @@ export type NumericKey = typeof NumericKey[number];
 export const FigureKey: (AlphabeticKey | NumericKey)[] = [...AlphabeticKey, ...NumericKey];
 export type FigureKey = typeof FigureKey[number];
 
-export const Accelerator: Accelerator[] = FigureKey
-  .map(figure => [
-    (figure: FigureKey): MetaFigureKey[] => MetaKey.map((meta): MetaFigureKey => `${meta}+${figure}`),
-    (figure: FigureKey): AltFigureKey[] => [`${AltKey}+${figure}`],
-    (figure: FigureKey): MetaShiftKey[] => MetaKey.map((meta): MetaShiftKey => `${meta}+${ShiftKey}+${figure}`),
-    (figure: FigureKey): AltShiftKey[] => [`${AltKey}+${ShiftKey}+${figure}`],
-    (figure: FigureKey): MetaAltFigureKey[] => MetaKey.map((meta): MetaAltFigureKey => `${meta}+${AltKey}+${figure}`),
-    (figure: FigureKey): MetaAltShiftFigureKey[] => MetaKey.map((meta): MetaAltShiftFigureKey => `${meta}+${AltKey}+${ShiftKey}+${figure}`),
-  ]
-    .map(accs =>
-      accs(figure)
-    )
-  )
-  .flat(2)
-  .sort();
-
-export type Accelerator = MetaFigureKey | AltFigureKey | MetaShiftKey | AltShiftKey | MetaAltFigureKey | MetaAltShiftFigureKey;
+export const MetaFigureKey: MetaFigureKey[] = FigureKey.map(figure => MetaKey.map((meta): MetaFigureKey => `${meta}+${figure}`)).flat(1).sort();
 export type MetaFigureKey = `${MetaKey}+${FigureKey}`;
+
+export const AltFigureKey: AltFigureKey[] = FigureKey.map((figure): AltFigureKey => `${AltKey}+${figure}`).sort();
 export type AltFigureKey = `${AltKey}+${FigureKey}`;
+
+export const MetaShiftKey: MetaShiftKey[] = FigureKey.map(figure => MetaKey.map((meta): MetaShiftKey => `${meta}+${ShiftKey}+${figure}`)).flat(1).sort();
 export type MetaShiftKey = `${MetaKey}+${ShiftKey}+${FigureKey}`;
+
+export const AltShiftKey: AltShiftKey[] = FigureKey.map((figure): AltShiftKey => `${AltKey}+${ShiftKey}+${figure}`).sort();
 export type AltShiftKey = `${AltKey}+${ShiftKey}+${FigureKey}`;
+
+export const MetaAltFigureKey: MetaAltFigureKey[] = FigureKey.map(figure => MetaKey.map((meta): MetaAltFigureKey => `${meta}+${AltKey}+${figure}`)).flat(1).sort();
 export type MetaAltFigureKey = `${MetaKey}+${AltKey}+${FigureKey}`;
+
+export const MetaAltShiftFigureKey: MetaAltShiftFigureKey[] = FigureKey.map(figure => MetaKey.map((meta): MetaAltShiftFigureKey => `${meta}+${AltKey}+${ShiftKey}+${figure}`)).flat(1).sort();
 export type MetaAltShiftFigureKey = `${MetaKey}+${AltKey}+${ShiftKey}+${FigureKey}`;
+
+export const Accelerator: (MetaFigureKey | AltFigureKey | MetaShiftKey | AltShiftKey | MetaAltFigureKey | MetaAltShiftFigureKey)[] = [...MetaFigureKey, ...AltFigureKey, ...MetaShiftKey, ...AltShiftKey, ...MetaAltFigureKey, ...MetaAltShiftFigureKey].sort();
+export type Accelerator = typeof Accelerator[number];
